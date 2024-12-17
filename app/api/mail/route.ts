584a7527ae530f6cc40dd877451363d7947158ce
ivan41kd <grkorolevskiy@gmail.com>
2024-12-17 11:58:46 +0300

    fix: route

--- app/api/mail/route.ts
+++ app/api/mail/route.ts
@@ -9,10 +9,9 @@ export async function POST(request: NextRequest) {
 		console.log(body);
 
 		const transport = nodemailer.createTransport({
-			service: 'yandex',
 			host: process.env.SMTP_HOST,
-			port: 465,
-			secure: true,
+			port: 587,
+			secure: false,
 			auth: {
 				user: process.env.SMTP_USER,
 				pass: process.env.SMTP_PASS,
